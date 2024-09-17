a621510d29db1234ba46362855bc18a8bbc3d4fd
Joshua Ascoli <100535893+JoshuaAscoli@users.noreply.github.com>
2024-09-17 15:38:42 -0600

    Ejercicio1

---
+++ EJ1.ts
@@ -0,0 +1,7 @@
+//Joshua Ascoli 1515522
+let number:number = 10;
+let string:string = 'Joshua';
+let boolean: boolean = false;
+console.log('Number type:', number);
+console.log('String type:', string);
+console.log('Boolean type:', boolean);
