d40d7ddc48c4ddd09fca278e567f11cf94c0bfb5
Joshua Ascoli <100535893+JoshuaAscoli@users.noreply.github.com>
2024-09-17 17:46:44 -0600

    Ejercicio10

---
+++ EJ10.ts
@@ -0,0 +1,7 @@
+let frase: string = 'Joshua Ascoli 1515522 en sistemas'
+let split_array1: string[] = frase.split('s')
+let split_array2: string[] = frase.split('',)
+let split_array3: string[] = frase.split(' ', 3)
+console.log(split_array1)
+console.log(split_array2)
+console.log(split_array3)
