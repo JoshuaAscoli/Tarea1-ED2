c172659741562b81bb996f10eee222247f0fb0c0
Joshua Ascoli <100535893+JoshuaAscoli@users.noreply.github.com>
2024-09-17 15:47:56 -0600

    Ejercicio2

---
+++ EJ2.ts
@@ -0,0 +1,15 @@
+//Joshua Ascoli 
+let number1: number = 10;
+let number2: number = 3; 
+// Operaciones
+let addition: number = number1 + number2;
+let subtraction: number = number1 - number2;
+let multiplication: number = number1 * number2;
+let divition: number = number1 / number2;
+let modulee: number = number1 % number2;
+
+console.log('Suma:', number1 ,'+', number2 ,'=', addition);
+console.log('Resta:', number1 ,'-', number2 ,'=', subtraction);
+console.log('Multiplicación:', number1 ,'*', number2 ,'=', multiplication);
+console.log('División:', number1 ,'/', number2 ,'=', divition);
+console.log('Módulo:', number1 ,'%', number2 ,'=', modulee);
