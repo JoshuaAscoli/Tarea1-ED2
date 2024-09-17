0538e9b37f51ff43635491d10323859fa10044f1
Joshua Ascoli <100535893+JoshuaAscoli@users.noreply.github.com>
2024-09-17 16:07:05 -0600

    Ejercicio4

---
+++ EJ4.ts
@@ -0,0 +1,14 @@
+//Joshua Ascoli 1515522
+console.log('Usando for:')
+for (let i = 1; i < 6; i++) {
+    console.log('Se imprime', i)
+}
+console.log('\n')
+
+let numero: number = 1; 
+console.log('Usando while:')
+while (numero < 8) {
+    console.log(numero, 'es menor que 8')
+    numero = numero + 1
+}
+console.log(numero, 'es igual a 8')
