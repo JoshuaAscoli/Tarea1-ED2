5576b8c485b7dcab7d3f189ab31439fa393ea614
Joshua Ascoli <100535893+JoshuaAscoli@users.noreply.github.com>
2024-09-17 15:54:35 -0600

    Ejercicio3

---
+++ EJ3.ts
@@ -0,0 +1,13 @@
+//Joshua Ascoli 1515522
+let num1: number = 1000;
+let num2: number = 45;
+
+if (num1>num2) {
+    console.log('El numero', num1,'es mayor que', num2)
+}
+
+else if (num1==num2) {
+    console.log('Los números son iguales')
+}
+
+else (console.log('El numero', num2,'es mayor que', num1));
